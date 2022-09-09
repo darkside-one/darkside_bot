5f929a54c7a72504e482c3b4f4a52a7e7874a0a1
Darkside <darkside.one@icloud.com>
2022-09-09 16:31:38 +0700

    first commit

---
+++ const.js
@@ -0,0 +1,17 @@
+const command = `
+/start - Перезапустить бота
+/help - Помощь
+/hello - NodeJS welcome you :)
+`
+
+const text1 = `
+1 Текст для проверки обработчика и <a href="https://youtube.com">ссылка </a>`
+const text2 = `
+2 Текст для проверки обработчика и <a href="https://youtube.com">ссылка </a>`
+const text3 = `
+3 Текст для проверки обработчика и <a href="https://youtube.com">ссылка </a>`
+
+module.exports.commands = command
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
